14c076a582d185ae4752e0c58d8523501aa14d72
Yunting Ma <yuntingm@usc.edu>
2022-11-23 01:31:15 +0000

    checkpoint

--- wordle.cpp
+++ wordle.cpp
@@ -11,12 +11,12 @@ using namespace std;
 
 // Add prototypes of helper functions here
 void wordle_helper(
-    const std::string in,
-    const std::string& floating,
+    const std::string& in,
+    std::string& floating,
     const std::set<std::string>& dict, 
-    std::set<std::string>& potential_set);
-
-void counting(std::string in, char c, int& count);
+    std::set<std::string>& potential_set,
+    std::string str,
+    int count);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -26,51 +26,56 @@ std::set<std::string> wordle(
 {
     // Add your code here
     std::set<std::string> potential_set;
-    wordle_helper(in, floating, dict, potential_set);
+    int dash = 0;//count for dash
+    for (int i=0; i<(int)in.length(); i++){
+      if (in[i] == '-') {
+        dash++;
+      }
+    }
+    std::string temp_floating = floating;
+    wordle_helper(in, temp_floating, dict, potential_set, "", dash);
     return potential_set;
 }
 
 // Define any helper functions here
-void counting(std::string temp, char letter, int& count) {
-	for (int i=0; i<(int)temp.length(); i++){
-    if (temp[i] == letter) {
-      count++;
-    }
-  }
-}
-
 void wordle_helper(
-    std::string in,
-    const std::string& floating,
+    const std::string& in,
+    std::string& floating,
     const std::set<std::string>& dict, 
-    std::set<std::string>& potential_set)
+    std::set<std::string>& potential_set,
+    std::string str,
+    int count)
 {
   //TODO
-  int dash = 0;//count for dash
-	counting(in, '-', dash);
-  if (dash == 0) {
-    //all filled, check floating exist in in
-    for (int i=0; i<(int)floating.length(); i++){
-      int float_count = 0;
-      int in_count = 0;
-			char temp_letter = floating[i];
-			counting(in, temp_letter, in_count);
-      counting(floating, temp_letter, float_count);
-      if (float_count > in_count) return;
+  if (str.size() == in.size()) {//no next position
+    if (floating.size() == 0) {
+      if (dict.find(str) != dict.end()) {
+        potential_set.insert(str);
+      }
     }
-    std::set<std::string>::iterator it = dict.find(in);
-    if(it != dict.end()) potential_set.insert(in);
   }
-  else {
-    //still have not sure char
-    for (int i=0; i<(int)in.length(); i++) {
-      if (in[i] == '-') {
-        for (char letter = 'a'; letter <= 'z'; letter++) {
-          in[i] = letter;
-          wordle_helper(in, floating, dict, potential_set);
-        }
+  else if (in[str.size()] != '-') {//next position is known
+    char letter = in[str.size()];
+    wordle_helper(in, floating, dict, potential_set, str+letter, count);
+  }
+  else {//next position == '-'
+    std::string potential_str = "abcdefghijklmnopqrstuvwxyz";
+    count -= 1;
+    for (std::string::iterator it=floating.begin(); it!=floating.end(); ++it){
+      char letter = *it;
+      floating.erase(it);
+      wordle_helper(in, floating, dict, potential_set, str+letter, count);
+      floating.insert(it, letter);
+      // if (potential_str.find(letter)!=potential_str.npos) {
+      //   potential_str.erase(potential_str.begin()+potential_str.find(letter));
+      // }
+    }
+    if (count >= int(floating.size())) {
+      for (std::string::iterator it=potential_str.begin(); it!=potential_str.end(); ++it) {
+        wordle_helper(in, floating, dict, potential_set, str+(*it), count);
       }
     }
   }
   return;
 }
+
