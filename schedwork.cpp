6f571fd3cbe39765f5161b8a6fc8e90048707d1c
Yunting Ma <yuntingm@usc.edu>
2022-11-29 07:57:31 +0000

    schedwork

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,7 +19,16 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool schedule_helper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& day_vec,
+    int current,
+    int row,
+    int col
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -35,9 +44,61 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
-
+    vector<Worker_T> temp = vector<Worker_T>(dailyNeed, 0);
+    for (int i=0; i<(int)avail.size(); i++) {
+        sched.push_back(temp);
+    }
+    vector<int> day_vec = vector<int>(avail[0].size(), 0);
+    int row = -1;
+    int col = avail[0].size()+1;
+		int current = 0;
+    return schedule_helper(avail, dailyNeed, maxShifts, sched, day_vec, current, row, col);
 }
+
+//helper function
+bool schedule_helper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& day_vec,
+    int current,
+    int row,
+    int col
+)
+{
+  //todo
+  if (row == (int)avail.size()) {
+    return true;
+  }
+  else if (col >= (int)dailyNeed) {
+    row ++;
+    col = 0;
+    current = 0;
+    return schedule_helper(avail, dailyNeed, maxShifts, sched, day_vec, current, row, col);
+  }
+  else if (current >= (int)avail[0].size()) {
+    return false;
+  }
+  else {
+    if ((int)maxShifts > day_vec[current]) {
+			if (avail[row][current]!=0) {
+				sched[row][col] = current;
+				day_vec[current] ++;
+        if (schedule_helper(avail, dailyNeed, maxShifts, sched, day_vec, current+1, row, col+1)) {
+          return true;
+        }
+				else {
+					sched[row][col] = -1;
+					day_vec[current] --;
+				}
+			}
+    }
+		current ++;
+		return schedule_helper(avail, dailyNeed, maxShifts, sched, day_vec, current, row, col);
+  }
+  
+}
+
+
 
